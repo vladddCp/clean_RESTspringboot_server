7bece6bae10c688220782376a9ea8536c1ce56da
vladddCp <39249705+vladddCp@users.noreply.github.com>
2019-03-26 16:17:03 +0200

    MySql impl.
    MySql impl.

--- src/main/java/server/Main.java
+++ src/main/java/server/Main.java
@@ -3,9 +3,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class Main {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(Main.class, args);
     }
 }
